3220cc7ea63161a6ade5d325a64915cbb4ef3c8d
Ankith Reddy <ankithreddy2001@gmail.com>
2020-10-21 21:33:59 +0530

    Update index.js
    added some extra info

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function result()
 		return;
 	}
 
-	document.getElementById('error').innerHTML =  "";
+	document.getElementById('error').innerHTML =  "Perfect!";
 
 
 	var result = document.getElementById('result');
