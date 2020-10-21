c88fea7ab1f6a0904a28d72c4a7ec97948a3b359
Ankith Reddy <ankithreddy2001@gmail.com>
2020-10-21 21:16:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+function result()
+{
+	
+	var list = document.getElementsByTagName('input');
+
+	var num1 = parseFloat(list[0].value);
+	var num2 = parseFloat(list[1].value);
+	
+	var operation = (list[2].value);
+
+	//console.log(operation);
+
+	var value = 0;
+
+	if(operation.localeCompare("*")===0)
+	{
+		value = num1*num2;
+	}
+	else if(operation.localeCompare("-")===0)
+	{
+		value = num1-num2;
+	}
+	else if(operation.localeCompare("/")===0)
+	{
+		if(num2===0)
+		{
+			value = "Division by Zero Not possible";
+		}
+		else
+		value = num1/num2;
+	}
+	else if(operation.localeCompare("%")===0)
+	{
+		value = num1%num2;
+	}
+	else if(operation.localeCompare("+")===0)
+	{
+		value = (num1 + num2);
+	}
+	else if(operation.localeCompare("^")===0)
+	{
+		value = Math.pow(num1,num2);
+	}
+	else
+	{
+		document.getElementById('error').innerHTML =  "Enter proper values and operations.";
+	}
+
+
+	var result = document.getElementById('result');
+
+
+	
+
+
+	result.value = value;
+
+}
